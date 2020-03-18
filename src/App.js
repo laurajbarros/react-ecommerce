299b964be44b51992cfeaaed4a5e2a9c935722ad
Laura Barros <laurajbarros@gmail.com>
2020-03-18 17:34:41 -0300

    added routing and links to other pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
 import { HomePage } from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>Hello World</div>
+)
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import './menu-item.styles.scss';
+import {withRouter} from 'react-router-dom';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div
-    style = {{
-      backgroundImage: `url(${imageUrl})`
-    }}
-    className={`${size} menu-item`}>
-    <div className='content'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
-      <span className='subtitle'>SHOP NOW</span>
+
+  const MenuItem = ({ title, imageUrl, size, history, linkUrl, match, location }) => (
+      <div
+      style = {{
+        backgroundImage: `url(${imageUrl})`
+      }}
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}>
+      <div className='content'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
     </div>
-  </div>
-)
+  );
 
-export default MenuItem;
+export default withRouter(MenuItem);
